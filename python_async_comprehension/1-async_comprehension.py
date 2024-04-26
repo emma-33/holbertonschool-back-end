d83a4088fae924bd5976d6f0563a9024677f9ae7
emma-33 <emma.lateyron@orange.fr>
2024-04-26 09:56:27 +0200

    [fix] 1-async_comprehension.py : add var annotation List[float]

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,12 @@
 """defines an coroutine called async_comprehension
 that takes no arguments."""
 import asyncio
-
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Collect 10 random numbers using an async comprehensing over
     an async generator"""
     return [i async for i in async_generator()]
