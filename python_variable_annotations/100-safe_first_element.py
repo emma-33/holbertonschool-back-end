78128b96519bea8e03f3fc388130f81d6ed41f19
emma-33 <emma.lateyron@orange.fr>
2024-04-26 14:47:45 +0200

    [fix] 100-safe_first_element.py

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Type-annotated function safe_first_element that takes a list lst of
 any type elements and returns the first element of the list."""
-from typing import Optional, Any, Sequence
+from typing import Optional, Any, Sequence, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Return the first element of lst if there is any, otherwise None"""
     if lst:
         return lst[0]
