b8d1a4d25bf79d6ee85a6ae4955a1fe2516f9c67
emma-33 <emma.lateyron@orange.fr>
2024-04-26 09:31:40 +0200

    [fix] all : #!/usr/bin/env python3

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """defines an coroutine called async_comprehension
 that takes no arguments."""
 import asyncio
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """Defines a coroutine called measure_runtime that
 measures the total runtime"""
 import asyncio
