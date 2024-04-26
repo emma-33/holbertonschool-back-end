a56b5bcfba327d6938c24ba0508815a443613185
emma-33 <emma.lateyron@orange.fr>
2024-04-26 09:53:21 +0200

    [fix] 0-async_generator.py : generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ that takes no arguments."""
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+
+async def async_generator() -> Generator[float, None, None]:
     """Yield a random number between 0 and 10 every second."""
     for i in range(10):
         await asyncio.sleep(1)
