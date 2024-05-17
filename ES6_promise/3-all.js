92c4b89a1a7575c648eb92c378a1ec89851da6b8
emma-33 <emma.lateyron@orange.fr>
2024-05-17 13:07:41 +0200

    [fix] 3-all.js : last name on different line

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,7 +4,8 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const { body } = values[0];
-      const { firstName, lastName } = values[1];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
