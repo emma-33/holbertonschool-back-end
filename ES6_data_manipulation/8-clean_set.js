014804d2f82f36d5f12d014138f4f9a8eb226015
emma-33 <emma.lateyron@orange.fr>
2024-05-22 00:23:13 +0200

    [fix] 8-clean_set.js : add if !startString and if element

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
   const newString = [];
 
-  if (startString.length === 0) {
+  if (!startString || startString.length === 0) {
     return '';
   }
 
   for (const element of set) {
-    if (element.startsWith(startString)) {
+    if (element && element.startsWith(startString)) {
       newString.push(element.slice(startString.length));
     }
   }
